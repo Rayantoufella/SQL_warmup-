2fa610f9816b75c3491ff6334f9f4d27f3ca6d33
Rayan Toufella <rayantoufella3@gmail.com>
2026-03-12 11:38:40 +0000

    Challenge 1 SQL is Done !

--- Challenge1.sql
+++ Challenge1.sql
@@ -1,23 +1,47 @@
--- ================================================
--- EXEMPLE DE CODE SQL BIEN FORMATÉ
--- ================================================
 
--- Créer une table pour les utilisateurs
-CREATE TABLE utilisateurs (
+CREATE DATABASE IF NOT EXISTS challenge1;
+
+
+USE challenge1;
+
+CREATE TABLE library_books (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(100) NOT NULL,
-    email VARCHAR(150) UNIQUE,
-    age INT,
-    date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    title VARCHAR(150) NOT NULL, 
+    author VARCHAR(100),
+    published_year YEAR,
+    status ENUM("Disponible", "Emprunter", "Perdu"),
+    price DECIMAL(10, 2)
 );
 
--- Insérer des données
-INSERT INTO utilisateurs (nom, email, age) VALUES
-('Jean Dupont', 'jean@example.com', 25),
-('Marie Martin', 'marie@example.com', 30);
 
--- Afficher les données
-SELECT * FROM utilisateurs;
 
--- Rechercher avec condition
-SELECT nom, email FROM utilisateurs WHERE age > 25;
+INSERT INTO library_books (id, title, author, published_year, status, price)
+VALUES 
+(1, "Book A", "Author A", 1950, "Disponible", 190.99),
+(2, "Book B", "Author B", 2020, "Disponible", 290.99),
+(3, "Book C", "Author B", 2023, "Disponible", 300.00),
+(4, "Book D", "Author C", 2010, "Emprunter", NULL),
+(5, "Book E", "Author D", 1995, "Perdu", 150.00),
+(6, "Book F", "Author E", 2015, "Disponible", 25.00),
+(7, "Book G", "Author F", 2026, "Disponible", 98.99),
+(8, "Book H", "Author G", 1990, "Emprunter", 20.00);
+
+
+SELECT author ,price FROM library_books;
+
+SELECT title , price FROM library_books WHERE price BETWEEN 100 AND 300 ;
+
+SELECT title , published_year FROM library_books WHERE published_year >= 2020 ;
+
+-- Afficher les titres en MAJUSCULE et le prix arrondi
+SELECT UPPER(title) AS titre_majuscule, ROUND(price) AS prix_arrondi FROM library_books;
+
+SELECT title FROM library_books WHERE title LIKE "%PHP%" ;
+    
+SELECT title FROM library_books WHERE status = "Perdu" ORDER BY published_year DESC ;
+
+SELECT DISTINCT author FROM library_books ;
+
+SELECT title FROM library_books WHERE UPPER(title) ;
+
+
